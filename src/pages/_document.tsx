672ac1dda43f2f95e41af3b6f1f3b295c9169fee
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2022-09-17 22:31:44 -0300

    feat: adicionando meta tags para SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,17 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
 
         <meta
           name="description"
           content="Front-End Engineer, Tecnólogo em Análise e Desenvolvimento de Sistemas e sempre buscando programar de maneira simples, organizada e com boas práticas de código."
+        />
+        <meta name="keywords" content="React, React Native, Vue, Node JS" />
+        <meta name="author" content="Clodoaldo Dantas" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <body>
