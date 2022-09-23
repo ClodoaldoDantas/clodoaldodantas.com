f62b20c6e80ba65b376ed304b1d351692ddd9665
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2022-09-22 22:00:48 -0300

    feat: alterando texto de qualificacoes

--- src/data/qualifications.ts
+++ src/data/qualifications.ts
@@ -4,6 +4,6 @@ const yearsOfExperiences = new Date().getFullYear() - START_IN_PROGRAMMING
 export const qualifications = [
   `${yearsOfExperiences} anos de experiência com desenvolvimento web.`,
   'Já trabalhei com diversos projetos de diferentes contextos.',
-  'Já trabalhei com os frameworks React, Angular e Vue.',
+  'Já trabalhei com os frameworks React, React Native e Vue.',
   'Consigo trabalhar muito bem em equipe e ajudar meus colegas.',
 ]
